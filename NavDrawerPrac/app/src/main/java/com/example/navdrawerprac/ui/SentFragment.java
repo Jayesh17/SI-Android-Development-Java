024404c1f6a48e85847f443442b6c725552736d0
Joy <jayeshjlalchandani@gmail.com>
2021-06-11 16:50:40 +0530

    registration page and forgot password page commit

---
+++ NavDrawerPrac/app/src/main/java/com/example/navdrawerprac/ui/SentFragment.java
@@ -0,0 +1,38 @@
+package com.example.navdrawerprac.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.navdrawerprac.R;
+
+public class SentFragment extends Fragment {
+
+    private SentViewModel mViewModel;
+
+    public static SentFragment newInstance() {
+        return new SentFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sent_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SentViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ NavDrawerPrac/app/src/main/java/com/example/navdrawerprac/ui/SentViewModel.java
@@ -0,0 +1,7 @@
+package com.example.navdrawerprac.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class SentViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
