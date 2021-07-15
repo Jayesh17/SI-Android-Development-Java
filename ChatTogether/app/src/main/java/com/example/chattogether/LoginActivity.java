d1066e3d56cfc1a070c92b6cb45222df60dfa483
Joy <jayeshjlalchandani@gmail.com>
2021-07-16 00:30:26 +0530

    Login and registration form design submitted.

--- ChatTogether/app/src/main/java/com/example/chattogether/LoginActivity.java
+++ ChatTogether/app/src/main/java/com/example/chattogether/LoginActivity.java
@@ -1,14 +1,127 @@
 package com.example.chattogether;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputLayout email;
+    TextInputLayout password;
+    TextInputEditText emailView;
+    TextInputEditText passView;
+
+    String Email;
+    String Password;
+
+    Button loginBtn;
+
+    private FirebaseAuth auth;
+
+    public void setInitialState()
+    {
+        emailView = findViewById(R.id.emailView);
+        passView = findViewById(R.id.passView);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        loginBtn = findViewById(R.id.loginBtn);
+    }
+
+    public boolean validateForm()
+    {
+        Email = emailView.getText().toString();
+        Password = passView.getText().toString();
+
+        if(Email.isEmpty())
+        {
+            email.setError("*This field is required.");
+            return false;
+        }
+        if(Email.indexOf('@')==-1)
+        {
+            email.setError("*Please enter valid Email Address.");
+            return false;
+        }
+
+        String numbers = "(.*[0-9].*)";
+        String lowerCaseChars = "(.*[a-zA-Z].*)";
+        if(Password.isEmpty())
+        {
+            password.setError("*This field is required.");
+            return false;
+        }
+        else if(Password.length() > 16 || Password.length() < 8)
+        {
+            password.setError("*Password must be of 8 to 16 characters.");
+            return false;
+        }
+        else if (!Password.matches(lowerCaseChars ))
+        {
+            password.setError("*Password must have at least one alphabet");
+            return false;
+        }
+        else if (!Password.matches(numbers ))
+        {
+            password.setError("*Password must have at least one number");
+            return false;
+        }
+        return true;
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setInitialState();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeErrors();
+                boolean isValidated = validateForm();
+                if(isValidated)
+                {
+                    //
+                    //Toast.makeText(getBaseContext(),"Verified",Toast.LENGTH_SHORT).show();
+                    auth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(LoginActivity.this,"Sign in successfull",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this,"Something went Wrong, please check network connection and Try again.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void removeErrors() {
+        email.setError(null);
+        password.setError(null);
+    }
+
+    public void redirectRegistration(View view)
+    {
+        startActivity(new Intent(getBaseContext(),RegistrationActivity.class));
+        finish();
     }
 }
--- ChatTogether/app/src/main/java/com/example/chattogether/RegistrationActivity.java
+++ ChatTogether/app/src/main/java/com/example/chattogether/RegistrationActivity.java
@@ -1,14 +1,165 @@
 package com.example.chattogether;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    TextInputLayout regEmail;
+    TextInputLayout regPassword;
+    TextInputLayout regName;
+    TextInputLayout cPassword;
+
+    TextInputEditText regEmailView;
+    TextInputEditText regNameView;
+    TextInputEditText regPassView;
+    TextInputEditText CPassView;
+
+    CircleImageView profileView;
+
+    String Email;
+    String Password;
+    String CPassword;
+    String Name;
+    int requestCode;
+
+    Button registerBtn;
+
+
+    public void setInitialState()
+    {
+        regEmail = findViewById(R.id.reg_email);
+        regName = findViewById(R.id.reg_name);
+        regPassword = findViewById(R.id.reg_password);
+        cPassword = findViewById(R.id.cpassword);
+
+        regEmailView = findViewById(R.id.regEmailView);
+        regPassView = findViewById(R.id.regPassView);
+        regNameView = findViewById(R.id.regNameView);
+        CPassView = findViewById(R.id.cpassView);
+        profileView = findViewById(R.id.profile_image);
+
+        registerBtn = findViewById(R.id.registerBtn);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        setInitialState();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeErrors();
+                boolean isValidated = validateForm();
+            }
+        });
+
+        profileView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent.createChooser(intent,"Title"),);*/
+                mGetContent.launch("image/*");
+            }
+        });
+    }
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    // Handle the returned Uri
+                    profileView.setImageURI(uri);
+                }
+            });
+
+
+    private void removeErrors() {
+        regEmail.setError(null);
+        regPassword.setError(null);
+        cPassword.setError(null);
+        regName.setError(null);
+    }
+
+    private boolean validateForm() {
+
+        Email = regEmailView.getText().toString();
+        Password = regPassView.getText().toString();
+        CPassword = CPassView.getText().toString();
+        Name = regNameView.getText().toString();
+
+        if(Email.isEmpty())
+        {
+            regEmail.setError("*This field is required.");
+            return false;
+        }
+        if(Email.indexOf('@')==-1)
+        {
+            regEmail.setError("*Please enter valid Email Address.");
+            return false;
+        }
+
+        String numbers = "(.*[0-9].*)";
+        String lowerCaseChars = "(.*[a-zA-Z ].*)";
+
+        if (Name.isEmpty())
+        {
+            regName.setError("*This field is Required.");
+            return false;
+        }
+        if(!Name.matches(lowerCaseChars))
+        {
+            regName.setError("*Enter Valid name.");
+            return false;
+        }
+        if(Password.isEmpty())
+        {
+            regPassword.setError("*This field is required.");
+            return false;
+        }
+        else if(Password.length() > 16 || Password.length() < 8)
+        {
+            regPassword.setError("*Password must be of 8 to 16 characters.");
+            return false;
+        }
+        else if (!Password.matches(lowerCaseChars ))
+        {
+            regPassword.setError("*Password must have at least one alphabet");
+            return false;
+        }
+        else if (!Password.matches(numbers ))
+        {
+            regPassword.setError("*Password must have at least one number");
+            return false;
+        }
+        if(!Password.equals(CPassword))
+        {
+            cPassword.setError("*Password And Confirm Password does not match.");
+            return false;
+        }
+        return true;
+    }
+
+    public void redirectLogin(View view)
+    {
+        startActivity(new Intent(getBaseContext(),LoginActivity.class));
+        finish();
     }
 }
